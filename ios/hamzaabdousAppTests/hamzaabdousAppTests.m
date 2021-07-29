11852851fac054b225c054496d79c55ab12923f6
hamzaabdous <abdoushamza6@gmail.com>
2021-07-29 18:24:48 +0100

    install app in my iphone

--- ios/hamzaabdousAppTests/hamzaabdousAppTests.m
+++ ios/hamzaabdousAppTests/hamzaabdousAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AppV1Tests : XCTestCase
+@interface hamzaabdousAppTests : XCTestCase
 
 @end
 
-@implementation AppV1Tests
+@implementation hamzaabdousAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
