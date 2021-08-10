29a2d81bddb1b98078c5e17a5d8501297d26d2d1
hamzaabdous <abdoushamza6@gmail.com>
2021-08-10 15:01:23 +0100

    add new map screen

---
+++ src/screens/Map.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {GlobalStyles} from '../../styles/Globalstyle';
+
+import {Text, View, FlatList, Button, TouchableOpacity} from 'react-native';
+
+export const Map = ({navigation}) => {
+  return (
+    <View style={{flex: 1}}>
+        
+    </View>
+  );
+};
